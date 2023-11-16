099ed9d3eb2d17b8fcda273c6bc23251b948709d
Sanders <84755843+Sanders2616@users.noreply.github.com>
2023-11-15 22:03:36 -0600

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,15 +3,12 @@ import dotenv from "dotenv"
 dotenv.config({path : "src/.env"})
 
 const dbConnection = new Sequelize (
-     "videojuego_200668",
-     "admin",
-     "1234",
-    //process.env.DB_NAME,
-    //process.env.DB_USER,
-    //process.env.DB_PASSWORD,
+     process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host : "localhost",
-        port: "3306",
+        host : process.env.HOST,
+        port: process.env.DB_PORT,
         dialect: "mysql"
     }
 )
